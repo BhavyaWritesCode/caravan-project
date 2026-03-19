54e04168f2a409faffeab61e09ac6b4ed9dade2e
Bhavya Saini <bhavyasaini06@gmail.com>
2026-03-19 16:50:41 +0530

    Initial project setup with Maven structure

---
+++ src/main/java/com/caravan/App.java
@@ -0,0 +1,13 @@
+package com.caravan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
